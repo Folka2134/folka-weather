96e4e67f22379dcc7a461d6775a897069cba685a
folka2134 <folka2134@gmail.com>
2024-06-05 12:59:24 +0100

    created weather map

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import NewsSection from "@/components/NewsSection";
 import SearchSection from "@/components/SearchSection";
+import WeatherMap from "@/components/WeatherMap";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <SearchSection />
       <NewsSection />
+      <WeatherMap />
     </main>
   );
 }
---
+++ components/WeatherMap.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+const WeatherMap = () => {
+  // const [mapData, setMapData] = useState("");
+
+  // useEffect(() => {
+  //   const fetchMap = async () => {
+  //     const res = await fetch(
+  //       `https://tile.openweathermap.org/map/temp_new/6/0/63.png?appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`,
+  //     );
+
+  //     const map = await res.blob();
+  //     const url = URL.createObjectURL(map);
+  //     setMapData(url);
+  //   };
+
+  //   fetchMap();
+  // });
+
+  return (
+    <div className="my-5 flex w-full flex-col px-5 md:px-12 lg:px-32 xl:px-44 2xl:px-80">
+      {/* {mapData && (
+        <Image src={mapData} alt="Weather Map" height={500} width={500} />
+      )} */}
+      <div className="bg-[#2A2A2A] p-3 text-white">
+        <h2 className="text-4xl">UK weather map</h2>
+        <p>View the weather with our interactive map</p>
+      </div>
+      <div className="h-60 bg-[url(../public/images/weathermap1.png)] bg-cover bg-no-repeat md:h-96" />
+      <div className="border-[1px] p-4 text-sm">
+        <span>Forecast for {Date().slice(0, 21)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherMap;
